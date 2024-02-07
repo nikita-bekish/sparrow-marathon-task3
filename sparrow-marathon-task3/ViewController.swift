0c72c53e18e8f7cd4988361e1d69cac12cb66c77
Nikita Bekish <bekish.nikita1997@gmail.com>
2024-02-07 22:19:25 +0300

    initial commit

---
+++ sparrow-marathon-task3/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  sparrow-marathon-task3
+//
+//  Created by Nikita Bekish on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
